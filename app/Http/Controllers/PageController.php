ebd2e54fcd6fb0161f2016b39ab6cd97cdef5cee
Julian David <julian.david.rengifo@correounivalle.edu.co>
2025-12-05 14:39:05 -0500

    se creo la carpeta pages en el views para llamar al home de la app y traer las preguntas como prubea

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use GuzzleHttp\Psr7\Query;
+use Illuminate\Http\Request;
+use App\Models\Question;   
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        $questions = Question::with('category', 'user')->latest()->get();
+
+        return view('pages.home', [
+            'questions' => $questions,
+        ]);
+    }    
+}
